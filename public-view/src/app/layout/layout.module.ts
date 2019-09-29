2c199af524fde1a576b6fa3a256738b5d38cd6ee
zubair saif <zubairsaif700@gmail.com>
2019-09-29 13:04:20 +0500

    ..

--- public-view/src/app/layout/layout.module.ts
+++ public-view/src/app/layout/layout.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
 
 import { LayoutRoutingModule } from "./layout-routing.module";
 import { LayoutComponent } from "./layout.component";
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { HomeComponent } from "./home/home.component";
-import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { ProfileDetailsComponent } from "./profile-details/profile-details.component";
 
 @NgModule({
   declarations: [
@@ -17,6 +18,11 @@ import { ProfileDetailsComponent } from './profile-details/profile-details.compo
     HomeComponent,
     ProfileDetailsComponent
   ],
-  imports: [CommonModule, LayoutRoutingModule, FormsModule]
+  imports: [
+    CommonModule,
+    LayoutRoutingModule,
+    AngularFontAwesomeModule,
+    FormsModule
+  ]
 })
 export class LayoutModule {}
