5753e42b753f1065202fa0ea17c55b1d2d166d64
zubair saif <zubairsaif700@gmail.com>
2019-10-11 20:17:51 +0500

    ..

--- public-view/src/app/layout/project/project.component.ts
+++ public-view/src/app/layout/project/project.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { BackendapiService } from "../../service/backendapi.service";
+import { ActivatedRoute, Router } from "@angular/router";
 @Component({
-  selector: 'app-project',
-  templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  selector: "app-project",
+  templateUrl: "./project.component.html",
+  styleUrls: ["./project.component.css"]
 })
 export class ProjectComponent implements OnInit {
+  constructor(
+    private backend: BackendapiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  createProfile(data) {
+    this.backend.createProject(data).subscribe(res => {
+      if (res) {
+        alert(res);
+      }
+    });
   }
-
+  ngOnInit() {}
 }
